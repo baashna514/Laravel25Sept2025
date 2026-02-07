1445212395ad732b794ef1728f730dbbf36ef45d
MoinAbbas <moinabbas80@gmail.com>
2026-02-07 12:34:06 +0500

    Email Code Has Been Commented

--- modules/Booking/Controllers/BookingController.php
+++ modules/Booking/Controllers/BookingController.php
@@ -636,13 +636,13 @@ public function confirmPayment(Request $request, $gateway)
         $contact->save();
 
         // Currency logic
-        $currency = in_array($booking->gateway, ['easypaisa', 'jazzcash']) ? 'Rs.' : '$';
-        $messageBody = "Order Has Been Confirmed & Payment of {$currency}{$booking->total} has been received against customer {$booking->email}.";
-
-        Mail::raw($messageBody, function ($message) use ($booking) {
-            $message->to('musamother@gmail.com')
-                ->subject('Order Confirmation');
-        });
+//        $currency = in_array($booking->gateway, ['easypaisa', 'jazzcash']) ? 'Rs.' : '$';
+//        $messageBody = "Order Has Been Confirmed & Payment of {$currency}{$booking->total} has been received against customer {$booking->email}.";
+//
+//        Mail::raw($messageBody, function ($message) use ($booking) {
+//            $message->to('musamother@gmail.com')
+//                ->subject('Order Confirmation');
+//        });
 
         return view('Booking::frontend/detail', $data);
     }
